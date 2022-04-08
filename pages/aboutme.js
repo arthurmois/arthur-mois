0637edcaa0a73ffa1aaa721971db2605f96132ab
Arthur Mois <arthurmois@gmail.com>
2022-04-07 20:15:17 -0700

    initial commit

---
+++ pages/aboutme.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <>
+      <h1 style={{ display: "flex", justifyContent: "center" }}>Hello Mom</h1>
+      <h3 style={{ display: "flex", justifyContent: "center" }}>It's me</h3>
+    </>
+  );
+}
